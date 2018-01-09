049a20ef02720dbedf0a8bdc006aeda69bc47da7
Eterna <one.eternity@gmail.com>
2018-01-09 08:39:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,16 +1,11 @@
 for (var i = 0; i < 25; i++) {
   if i=1 {
-    
+  console.log("I am 1 strange loop.")
   }
   else
   
   console.log("I am ${i} strange loops.");
 }
-
-
-
-
-
 
 
 let countdown = 
